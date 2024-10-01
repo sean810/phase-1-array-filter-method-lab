2987bcb47338a88d09cbea7fe307c01561ace71f
sean810 <seantashel@gmail.com>
2024-10-01 11:03:56 +0300

    complete tab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+const drivers = [
+    { name: "Carti" },
+    { name: "Ken" },
+    { name: "lone" },
+    { name: "Beno" },
+    { name: "meechie" },
+    { name: "Problem child" },
+    { name: "Lil Demon" },
+];
